0c3aa8ee94b6884d69bec2a273905d7843f5f65c
raveit65 <raveit65.sun@gmail.com>
2026-01-12 23:31:14 +0100

    do not use mate-autogen to generate build files

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mate-themes"
+PKG_NAME="some-gtk3-themes"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
@@ -12,13 +12,10 @@ PKG_NAME="mate-themes"
     exit 1
 }
 
-which mate-autogen || {
-    echo "You need to install mate-common from the MATE Git"
-    exit 1
-}
-
 REQUIRED_AUTOMAKE_VERSION=1.9
-USE_COMMON_DOC_BUILD=yes
 
-. mate-autogen
+autoreconf -fi &&
+
+./configure
+
 
